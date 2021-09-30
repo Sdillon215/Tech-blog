bbbd5e9b75249ac6717787e6dbfa9592ffac9d3c
Sean <sdillon215@gmail.com>
2021-09-29 21:35:42 -0700

    user routes. Get all, get by id, update, and delete routes all tested

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
