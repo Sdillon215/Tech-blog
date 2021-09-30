cfefac22306f66cb483171d291bc20d7773cf709
Sean <sdillon215@gmail.com>
2021-09-29 20:20:17 -0700

    sequelize connection

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening'));
+});
