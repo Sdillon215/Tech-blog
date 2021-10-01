a7cad5efe4c00775f6b82ac9bedcf1afb70c38db
Sean <sdillon215@gmail.com>
2021-10-01 13:52:37 -0700

    stylesheet added route tested

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
