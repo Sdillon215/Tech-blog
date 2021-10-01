1c3cd91e7ee53285e597756306abb048c33f9fc0
Sean <sdillon215@gmail.com>
2021-10-01 16:49:24 -0700

    login/signup forms/fetch calls working

---
+++ public/javascript/login.js
@@ -0,0 +1,51 @@
+$('#login-submit').click(async function (e) {
+    e.preventDefault();
+    console.log('login click');
+
+    const email = $('#email-login').val().trim();
+    const password = $('#password-login').val().trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+});
+
+$('#signup-submit').click(async function (e) {
+    e.preventDefault();
+
+    const username = $('#username-signup').val().trim();
+    const email = $('#email-signup').val().trim();
+    const password = $('#password-signup').val().trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+});
+
