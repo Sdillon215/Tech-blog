ab4fe9ca8c5d4bbf0319d04985ae8ea2f9be08d7
Sean <sdillon215@gmail.com>
2021-10-01 11:27:18 -0700

    login route created, password verification

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -75,34 +75,29 @@ router.post('/', (req, res) => {
         });
 });
 
-// router.post('/login', (req, res) => {
-//     User.findOne({
-//         where: {
-//             email: req.body.email
-//         }
-//     }).then(dbUserData => {
-//         if (!dbUserData) {
-//             res.status(400).json({ message: 'No user with that email address!' });
-//             return;
-//         }
+router.post('/login', (req, res) => {
+    // expects email: email@gmail.com, password: password
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No user with that email.' });
+                return;
+            }
 
-//         const validPassword = dbUserData.checkPassword(req.body.password);
+            const validPassword = dbUserData.checkPassword(req.body.password);
 
-//         if (!validPassword) {
-//             res.status(400).json({ message: 'Incorrect password!' });
-//             return;
-//         }
-
-//         req.session.save(() => {
-//             // declare session variables
-//             req.session.user_id = dbUserData.id;
-//             req.session.username = dbUserData.username;
-//             req.session.loggedIn = true;
-
-//             res.json({ user: dbUserData, message: 'You are now logged in!' });
-//         });
-//     });
-// });
+            if (!validPassword) {
+                res.status(400).json({ message: 'Incorrect password!' });
+                return;
+            }
+            
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        })
+})
 
 // router.post('/logout', withAuth, (req, res) => {
 //     if (req.session.loggedIn) {
