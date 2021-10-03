efa9a4ed17d3129f6c5b44e3ea3d3c57d01a5570
Sean <sdillon215@gmail.com>
2021-10-02 17:57:59 -0700

    delete post

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,17 @@
+$('#delete').click(async function (e) {
+    e.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    }); 
+    
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+});
